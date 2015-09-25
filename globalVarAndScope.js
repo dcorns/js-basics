a13089d97b0b3441d277f60b44d812174cad67f3
Dale Corns <drcorns@hotmail.com>
2015-09-25 14:33:28 -0700

    fix line number logging

--- globalVarAndScope.js
+++ globalVarAndScope.js
@@ -10,53 +10,53 @@ var bucket;
 function addToBucket(bucket){
   bucket++;
 }
-console.log('15: ' + addToBucket(bucket));
+console.log('13: ' + addToBucket(bucket));
 
 bucket = 0;
-console.log('19: ' + addToBucket(bucket));
+console.log('16: ' + addToBucket(bucket));
 
 var bucket1 = 0;
-console.log('23: ' + addToBucket(bucket1));
-console.log('14: ' + addToBucket(bucket));
+console.log('19: ' + addToBucket(bucket1));
+console.log('20: ' + addToBucket(bucket));
 
 bucket = 0;
 
-console.log('18: ' + addToBucket(bucket));
+console.log('24: ' + addToBucket(bucket));
 
 var bucket1 = 0;
 
-console.log('22: ' + addToBucket(bucket1));
+console.log('28: ' + addToBucket(bucket1));
 
-//function addToBucket2(bucket){
-//  bucket++;
-//  return bucket;
-//}
-//console.log('30: ' + addToBucket2(bucket1));
-//console.log('31: ' + addToBucket2(bucket));
-//console.log('32: ' + bucket1);
-//console.log('33: ' + bucket);
+function addToBucket2(bucket){
+  bucket++;
+  return bucket;
+}
+console.log('34: ' + addToBucket2(bucket1));
+console.log('35: ' + addToBucket2(bucket));
+console.log('36: ' + bucket1);
+console.log('37: ' + bucket);
 //function addToBucket2(bucket){
 //  bucket++;
 //}
 //
-//console.log('37: ' + addToBucket2(bucket1));
-//console.log('38: ' + addToBucket2(bucket));
+//console.log('42: ' + addToBucket2(bucket1));
+//console.log('43: ' + addToBucket2(bucket));
 //function addToBucket3(){
 //  bucket++;
 //  bucket1++;
 //}
 //addToBucket3();
-//console.log('47: ' + bucket1);
-//console.log('48: ' + bucket);
+//console.log('49: ' + bucket1);
+//console.log('50: ' + bucket);
 function addToBucket4(){
   bucket3 = 0;
   bucket3++;
 }
 //console.log('58: ' + bucket3);
 addToBucket4();
-console.log('66: ' + bucket3);
+console.log('57: ' + bucket3);
 
 function logBucket3(bucket3){
-  console.log('72: ' + bucket3);
+  console.log('60: ' + bucket3);
 }
 logBucket3();
