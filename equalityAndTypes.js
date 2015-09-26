1969804390dac3cec8f526039d1d1349a9575320
Dale Corns <drcorns@hotmail.com>
2015-09-26 16:02:50 -0700

    add more documentation

--- equalityAndTypes.js
+++ equalityAndTypes.js
@@ -19,13 +19,24 @@ function start(){
   console.log(n / nn);
   console.log(n - nn);
   console.log(n + nn);
+  /**
+   *=== does strong type checking == does not
+   */
   (9 == n) ? console.log('9 == n') : console.log('9 != n');
   (9 === n) ? console.log('9 == n') : console.log('9 != n');
+  /**
+   * Comparing strings and numbers
+   */
   ('A' > 'a') ? console.log('A > a') : console.log('A < a');
   ('A' > 'Z') ? console.log('A > Z') : console.log('A < Z');
   ('A' > '26') ? console.log('A > 26') : console.log('A < 26');
   ('a' > 'z') ? console.log('a > z') : console.log('a < z');
   ('9' > '3') ? console.log('\'9\' > \'3\'') : console.log('\'9\' < \'3\'');
+  ('9' > 3) ? console.log('\'9\' > \'3\'') : console.log('\'9\' < \'3\'');
+  (9 > '3') ? console.log('\'9\' > \'3\'') : console.log('\'9\' < \'3\'');
+  /**
+   * True/False values
+   */
   (NaN) ? console.log('NaN is true') : console.log('NaN is false');
   (undefined) ? console.log('undefined is true') : console.log('undefined is false');
   (null) ? console.log('null is true') : console.log('null is false');
@@ -36,7 +47,9 @@ function start(){
   (25) ? console.log('25 is true') : console.log('25 is false');
 }
 start();
-
+/**
+ * Types of types
+ */
 function andTheTypes(){
   console.log(typeof {});
   console.log(typeof undefined);
@@ -46,7 +59,15 @@ function andTheTypes(){
   console.log(typeof 5);
   console.log(typeof.314);
   console.log(typeof []);
+  /**
+   * Checking for arrays
+   */
   console.log(Array.isArray([]));
+  /**
+   * Constructors make of their own type, this is evident in Dev Tools, but JS typeof returns only object
+   * @param color
+   * @constructor
+   */
   var Bean = function(color){
     this.color = color;
   };
