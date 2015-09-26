b8619617a62914868fb7940f479f6fc507e9982d
Dale Corns <drcorns@hotmail.com>
2015-09-26 16:04:44 -0700

    remove comments from master

--- equalityAndTypes.js
+++ equalityAndTypes.js
@@ -6,9 +6,6 @@
  */
 'use strict';
 function start(){
-  /**
-   *Multiplication, division and subtraction convert strings to numbers. Addition will concat them.
-   */
   var n = '9', nn = 3;
   console.log(n * nn);
   console.log(n / nn);
@@ -19,14 +16,8 @@ function start(){
   console.log(n / nn);
   console.log(n - nn);
   console.log(n + nn);
-  /**
-   *=== does strong type checking == does not
-   */
   (9 == n) ? console.log('9 == n') : console.log('9 != n');
   (9 === n) ? console.log('9 == n') : console.log('9 != n');
-  /**
-   * Comparing strings and numbers
-   */
   ('A' > 'a') ? console.log('A > a') : console.log('A < a');
   ('A' > 'Z') ? console.log('A > Z') : console.log('A < Z');
   ('A' > '26') ? console.log('A > 26') : console.log('A < 26');
@@ -34,9 +25,6 @@ function start(){
   ('9' > '3') ? console.log('\'9\' > \'3\'') : console.log('\'9\' < \'3\'');
   ('9' > 3) ? console.log('\'9\' > \'3\'') : console.log('\'9\' < \'3\'');
   (9 > '3') ? console.log('\'9\' > \'3\'') : console.log('\'9\' < \'3\'');
-  /**
-   * True/False values
-   */
   (NaN) ? console.log('NaN is true') : console.log('NaN is false');
   (undefined) ? console.log('undefined is true') : console.log('undefined is false');
   (null) ? console.log('null is true') : console.log('null is false');
@@ -59,15 +47,7 @@ function andTheTypes(){
   console.log(typeof 5);
   console.log(typeof.314);
   console.log(typeof []);
-  /**
-   * Checking for arrays
-   */
   console.log(Array.isArray([]));
-  /**
-   * Constructors make of their own type, this is evident in Dev Tools, but JS typeof returns only object
-   * @param color
-   * @constructor
-   */
   var Bean = function(color){
     this.color = color;
   };
