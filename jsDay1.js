50fc9259110f1795b16c20cfd81053ff886fa930
Dale Corns <drcorns@hotmail.com>
2015-09-28 10:02:54 -0700

    add day one comments

---
+++ jsDay1.js
@@ -0,0 +1,46 @@
+/**
+ * jsDay1
+ * Created by dcorns on 9/28/15
+ * Copyright © 2015 Dale Corns
+ */
+'use strict';
+
+//Console.log Console.dir
+
+//var case insensitive start with underscore or letter
+
+//Assignment Operators =, +=, **=, <<=, &=, |=
+
+//Comparison Operators ==, ===, !=. !==, <, >, >=, <=
+
+//Arithmetic Operators %, ++, --, **
+
+//Bitwise operators &, |, ^, ~, <<, >>
+
+//Logical operators &&, ||, !
+
+//String operator +
+
+//ternary operator () ? true : false
+
+// , operator
+
+// delete(returns bool), typeof
+
+//relational operators: in (property in object), instanceof (obj instanceof object type)
+
+//keywords: this refers to calling object
+
+//Base Numbers 0b, 0x, 0
+
+//Highlighted objects Number, Math, Date, Array
+
+//Escaping characters in strings \' \n
+
+//Loops while, do, for, for in, break, continue
+
+//functions: closures dependencies, tying functions into the DOM
+
+//Objects: creating, constructor, Analyzing DOM Objects
+
+
